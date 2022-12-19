7b56b865d9d756e12b72f677ecbb8b9dd24c929a
DRGWeiss <ruizd1710@gmail.com>
2022-12-19 16:45:55 -0500

    sampel route testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const playedGameController = require('../controllers/playedGame')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', playedGameController.index);
 
 module.exports = router;
