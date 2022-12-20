ac599f2edc04c87d083435dc8d98bfac47d4cbee
DRGWeiss <ruizd1710@gmail.com>
2022-12-19 21:45:49 -0500

    connect to database

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
-const { connectDB, connectToServer } = require('./services/mongodb')
+const { connectDB } = require('./mongodb')
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
