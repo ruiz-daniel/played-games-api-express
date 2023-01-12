a73fc732fde75f21f78760497caeca8d8b3586c9
DRGWeiss <ruizd1710@gmail.com>
2023-01-12 18:03:18 -0500

    implement authentication on playedgames route

--- routes/playedGames.js
+++ routes/playedGames.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var authenticate = require('../authenticateMiddleware')
 
 const playedGameController = require('../controllers/playedGame')
 
 
-router.get('/', playedGameController.getPlayedGames);
-router.get('/:id', playedGameController.getPlayedGamesById);
-router.post('/', playedGameController.createPlayedGame);
+router.get('/', authenticate.authenticateToken, playedGameController.getPlayedGames);
+router.get('/:id', authenticate.authenticateToken, playedGameController.getPlayedGamesById);
+router.post('/', authenticate.authenticateToken, playedGameController.createPlayedGame);
 
 module.exports = router;
