765aa4e2d13edc55a6681bdee590691f81bee347
DRGWeiss <ruizd1710@gmail.com>
2023-01-12 20:19:23 -0500

    fix: populate fields when getting game by user and id

--- services/playedGame.js
+++ services/playedGame.js
@@ -7,23 +7,31 @@ const playedGameModel = require('../models/playedGame')
  */
 module.exports.handler = {
   async get() {
-    const result = await playedGameModel.find().populate(['user', 'completion', 'platform'])
+    const result = await playedGameModel
+      .find()
+      .populate(['user', 'completion', 'platform'])
     return result
   },
   async getById(id) {
-    const result = await playedGameModel.findById(id).catch(error => {
-      throw new Error('Game not Found')
-    })
+    const result = await playedGameModel
+      .findById(id)
+      .populate(['user', 'completion', 'platform'])
+      .catch((error) => {
+        throw new Error('Game not Found')
+      })
     return result
   },
   async getByUser(user) {
-    const result = await playedGameModel.find(game => game.user === user).catch(error => {
-      throw new Error(error.message)
-    })
+    const result = await playedGameModel
+      .find({ user })
+      .populate(['user', 'completion', 'platform'])
+      .catch((error) => {
+        throw new Error(error.message)
+      })
     return result
   },
   async create(playedGame) {
     const result = await playedGameModel.create(playedGame)
     return result
-  }
+  },
 }
