2ee2c625e018ca61f1821f6ea809763c9fef6d0c
DRGWeiss <ruizd1710@gmail.com>
2023-01-12 19:40:42 -0500

    allowing cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const cors = require('cors')
 
 var usersRouter = require('./routes/users')
 var platformsRouter = require('./routes/platforms')
@@ -15,6 +16,9 @@ var app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
+
+
+app.use(cors())
 
 app.use(logger('dev'))
 app.use(express.json())
