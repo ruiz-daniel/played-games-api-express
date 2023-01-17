9496b04d17e34101c2f0ddd80d80f0714d045edb
DRGWeiss <ruizd1710@gmail.com>
2023-01-17 08:29:34 -0500

    user model code formatting

--- models/user.js
+++ models/user.js
@@ -2,19 +2,20 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 
 const userSchema = new Schema({
-  username: {type: String, required: true, unique: true},
-  display_name: String,
-  admin: {type: Boolean, default: false},
-  premium: {type: Boolean, default: false},
+  username: { type: String, required: true, unique: true },
+  display_name: { type: String, required: true },
+  email: String,
+  admin: { type: Boolean, default: false },
+  premium: { type: Boolean, default: false },
   profile_picture: String,
-  password: {type: String, required: true, virtual: true},
-  disabled: {type: Boolean, default: false}
+  password: { type: String, required: true, virtual: true },
+  disabled: { type: Boolean, default: false },
 })
 
-userSchema.methods.toJSON = function() {
- var obj = this.toObject();
- delete obj.password;
- return obj;
+userSchema.methods.toJSON = function () {
+  var obj = this.toObject()
+  delete obj.password
+  return obj
 }
 
 module.exports = mongoose.model('user', userSchema)
