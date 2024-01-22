73301dd19a54397f40d37f30ef52a6e9c69155a7
DRGWeiss <ruizd1710@gmail.com>
2024-01-22 07:18:37 -0500

    authenticate user endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,14 @@
 const express = require('express')
 var router = express.Router()
 const userController = require('../controllers/user')
+var authenticate = require('../authenticateMiddleware')
 
 /* GET users listing. */
-router.get('/', userController.getUsers)
-router.get('/:id', userController.getUser)
+router.get('/', authenticate.authenticateToken, userController.getUsers)
+router.get('/:id', authenticate.authenticateToken, userController.getUser)
 router.post('/login', userController.login)
 router.post('/', userController.register)
-router.patch('/', userController.update)
-router.delete('/:id', userController.delete)
+router.patch('/', authenticate.authenticateToken, userController.update)
+router.delete('/:id', authenticate.authenticateToken, userController.delete)
 
 module.exports = router
