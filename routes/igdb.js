a50b76ccc60c49a3f4c4f71a0e9c5121e5d8775c
Daniel <daniel.ruiz@chartedhealth.com>
2026-08-15 13:06:16 -0400

    fix get by name url

--- routes/igdb.js
+++ routes/igdb.js
@@ -4,6 +4,6 @@ var router = express.Router();
 const igdbController = require("../controllers/igdb");
 
 router.get("/credentials", igdbController.getCredentials);
-router.get("/:name/:access_token", igdbController.getByName);
+router.get("/:name", igdbController.getByName);
 
 module.exports = router;
