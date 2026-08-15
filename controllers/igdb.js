8fae5417ef42e484a78f65a3aa63e38bc3e4e4a8
Daniel <daniel.ruiz@chartedhealth.com>
2026-08-15 10:57:56 -0400

    update igdb games endpoint route

--- controllers/igdb.js
+++ controllers/igdb.js
@@ -1,24 +1,25 @@
 // import { handler } from "../services/igdb"
-const igdbService = require("../services/igdb")
+const igdbService = require("../services/igdb");
 
 exports.getCredentials = async (req, res, next) => {
-  const response = await igdbService.handler.getCredentials().catch(error => {
-      res.status(400)
-      return error
-    })
-    res.send(response)
-}
+  const response = await igdbService.handler.getCredentials().catch((error) => {
+    res.status(400);
+    return error;
+  });
+  res.send(response);
+};
 
 exports.getByName = async (req, res, next) => {
-  const name = req.params.name
-  const bearerToken = req.body.access_token
+  const name = req.params.name;
+  const bearerToken = req.params.access_token;
   if (bearerToken && name) {
-    const response = await igdbService.handler.getByName(name, bearerToken).catch(error => {
-      res.status(400)
-      return error
-    })
-    res.send(response)
+    const response = await igdbService.handler
+      .getByName(name, bearerToken)
+      .catch((error) => {
+        res.status(400);
+        return error;
+      });
+    res.send(response);
   }
-  res.status(400)
-}
-
+  res.status(400);
+};
--- routes/igdb.js
+++ routes/igdb.js
@@ -1,9 +1,9 @@
-const express = require('express')
-var router = express.Router()
+const express = require("express");
+var router = express.Router();
 
-const igdbController = require('../controllers/igdb')
+const igdbController = require("../controllers/igdb");
 
-router.get('/credentials', igdbController.getCredentials)
-router.get('/:name', igdbController.getByName)
+router.get("/credentials", igdbController.getCredentials);
+router.get("/:name/:access_token", igdbController.getByName);
 
-module.exports = router
+module.exports = router;
