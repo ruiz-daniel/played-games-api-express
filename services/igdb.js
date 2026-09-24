9176b32f755c9586d4275055aef97b39c9069c6b
Daniel <daniel.ruiz@chartedhealth.com>
2026-09-23 21:29:49 -0400

    testing

--- services/igdb.js
+++ services/igdb.js
@@ -54,6 +54,7 @@ module.exports.handler = {
         game.cover.url = game.cover.url.replace("t_thumb", "t_1080p");
       }
       game.query = `fields ${getGameFields}; where ${gameNameQuery} & (${gameTypesQuery}); limit ${limit};`;
+      game.exact = exact;
     });
     return data;
   },
